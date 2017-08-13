11f4d6abd6165aaa6231cd0cbbbaeb4fc7a4e129
Ayush Singla <ayush@exunclan.com>
2017-08-13 13:23:18 +0530

    Fix and Update hash_table.hpp

--- src/cpp/hash_table.hpp
+++ src/cpp/hash_table.hpp
@@ -1,27 +1,49 @@
-#ifndef HASH_TABLE_H
+//  Copyright © 2017 Exun Clan. All rights reserved.
+#ifndef HASH_TABLE_H // _BRANCH_PRACTICE_SRC_CPP_HASH_TABLE_HPP
 #define HASH_TABLE_H
 
-struct node {
+#define MAXN static_cast<int>(1e6+1)
 
+struct node {
+  int key, val;
+  node *next;
+  node() {
+    key = val = 0;
+    next = nullptr;
+  }
 };
 
 struct list {
-
+  node *head;
 };
 
 struct hash_table {
-
+  int current_size;
+  int nods;
+  list table[MAXN];
+  explicit hash_table(int size) {
+    current_size = size;
+    nods = 0;
+    for (int i = 0; i < current_size; i++)
+      table[i].head = nullptr;
+  }
 };
 
-int hash(int key, int s); // computes the hash function of the key given s, the size of the table
+// computes the hash function of the key given s, the size of the table
+int hash(int key, int s);
+// returns pointer to node of key if it exists else NULL
+node *hash_contains(hash_table *t, int key);
+// deletes key from hash table if it exists
+void hash_delete(hash_table *t, int key);
+// insert key into hash table
+void hash_insert(hash_table *t, int key, int value);
+// gets the value of the key in the hash table, NULL if it doesn't exist
+int hash_get(hash_table *t, int key);
+// initialise the table
+void hash_init(hash_table **t, int s);
+// test the table's functionality
+void test(hash_table **t);
+// pretty print the table, print the table visually as a table
+void pprint(hash_table *t);
 
-node *hash_contains(hash_table *t, int key); // returns pointer to node of key if it exists else NULL 
-void hash_delete(hash_table *t, int key); // deletes key from hash table if it exists
-void hash_insert(hash_table *t, int key, int value); // insert key into hash table
-int hash_get(hash_table *t, int key); // gets the value coressponding to the key in the hash table, NULL if it doesn't exist
-void hash_init(hash_table **t, int s); // initialise the table
-void test(hash_table **t); // test the table's functionality
-void pprint(hash_table *t); // pretty print the table, print the table visually as a table
-
-
-#endif
+#endif // _BRANCH_PRACTICE_SRC_CPP_HASH_TABLE_HPP
